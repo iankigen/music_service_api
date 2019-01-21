0bbd7783fb7a751587854b7621a668b9281b8c31
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-01-21 17:34:29 +0300

    chore: add songs model

--- music/models.py
+++ music/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Songs(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    artist = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return '<Songs: {} - {}'.format(self.title, self.artist)
