f51c8576936cadc31cb861c88f9b335f269cab54
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-01-21 17:03:05 +0300

    chore: add env example

--- music_service_api/settings/base.py
+++ music_service_api/settings/base.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+print('*'*10)
+print('*', os.environ)
+print('*'*10)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-+)uxxvo2-xr)5$as!-8+)qu6vy#!x3^o4=z9osql#0i#-j*24'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
