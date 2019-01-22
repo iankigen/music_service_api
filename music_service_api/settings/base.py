fbeab48dd1abfe26ce1f794b9e69e429ae44a5d4
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-01-22 19:14:57 +0300

    chore: add versioning

--- music_service_api/settings/base.py
+++ music_service_api/settings/base.py
@@ -113,3 +113,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # When you enable API versioning, the request.version attribute will contain a string
+    # that corresponds to the version requested in the incoming client request.
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
+}
